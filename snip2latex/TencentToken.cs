d2ae342ddd74d50d2ae1f82fa865be9e64fa1317
RzBu11d023R <18226542+PanJunzhong@users.noreply.github.com>
2020-05-22 22:47:42 +0800

    begin Tencent ver dev

---
+++ snip2latex/TencentToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snip2latex
+{
+    public static class TencentToken
+    {
+        public static readonly string secrectId;
+        public static readonly string secrectKey;
+
+
+    }
+}
